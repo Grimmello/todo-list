3ff74679442cf3aa4db39c773785adcbc746452c
Clifford Grimmell <agrimmell2@gmail.com>
2017-02-27 15:17:37 -0800

    create html, js, img, and css folder

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Business Logic
+function todoList(list) {
+  this.item = list;
+}
+
+$(function() {
+  $("form#todo").submit(function(event) {
+    event.preventDefault();
+    var listItem = $("input#todoItem").val();
+    var addItem = new todoList(listItem);
+
+// Front End Logic
+    $("ul#list-1").append("<li><span class='listItems'>"+ addItem.item +"</span></li>");
+
+    $("li").on('click', function(){
+      var element = this;
+      $("ul#list-2").append(element);
+      console.log(element);
+      $(this).hide();
+      $(element).show();
+    });
+  });
+});
